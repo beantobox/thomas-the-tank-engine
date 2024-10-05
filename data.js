93e6f4f43c1bb03041a4238f899186166d31a498
beantobox <sawyoneyons@gmail.com>
2024-10-05 14:34:36 +0800

    thomas commit
    thomas the tank engineering department tour

---
+++ data.js
@@ -0,0 +1,158 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.22273277966857918,
+        "pitch": -0.010004761582845134,
+        "fov": 1.4398647332081382
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07632852070149454,
+          "pitch": -0.28440047798992474,
+          "rotation": 7.0685834705770345,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.17812698262554072,
+          "pitch": 0.08037082701205556,
+          "title": "engeeenierng",
+          "text": "60rp&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.3504646349200495,
+        "pitch": -0.3143004920032748,
+        "fov": 1.4398647332081382
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12730583961681674,
+          "pitch": -0.016881710059692523,
+          "rotation": 6.283185307179586,
+          "target": "2-cafe-and-nerds-studying"
+        },
+        {
+          "yaw": -0.9367328485763977,
+          "pitch": 0.0640974678925037,
+          "rotation": 5.497787143782138,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.350387704920049,
+          "pitch": -0.3142235620032743,
+          "title": "pasta statue",
+          "text": "how much of our school fees and taxpayer money went into this ah"
+        }
+      ]
+    },
+    {
+      "id": "2-cafe-and-nerds-studying",
+      "name": "cafe and nerds studying",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.5267602450747084,
+        "pitch": -0.08191372648126105,
+        "fov": 1.3255432192214336
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2828491228092176,
+          "pitch": 0.09554649502656787,
+          "rotation": 0,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4773186766320858,
+          "pitch": -0.20546559688648358,
+          "title": "spinelli coffee company",
+          "text": "rip"
+        },
+        {
+          "yaw": -0.5049400136016438,
+          "pitch": -0.03552039747339464,
+          "title": "nerds",
+          "text": "haha studying zzz typical nus"
+        }
+      ]
+    }
+  ],
+  "name": "thomas the tank engineering tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
